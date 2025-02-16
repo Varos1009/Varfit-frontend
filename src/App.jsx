5d9cee0a3f0efc4da43ca9c2fc056988979be9dd
Varos1009 <varos1009@gmail.com>
2025-02-16 21:26:00 +0100

    Created front app and tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import '../src/index.css'
+
+function App() {
+
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold text-blue-500">Tailwind is Working! 🚀</h1>
+    </div>
+  )
+}
+
+export default App
