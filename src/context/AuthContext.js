1745106c96a1492fb8e97f4c85f55f0b8f4666dc
Varos1009 <varos1009@gmail.com>
2025-02-19 11:13:25 +0100

    Firebase setup

---
+++ src/context/AuthContext.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error('useAuth must be used within an AuthProvider');
+    }
+    return context;
+};
+
+ const AuthProvider=({ children })=> {
+
+    const [currentUser, setCurrentUser] = useState(null);
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, initialUser);
+        return unsubscribe;
+    }, []);
+
+    async function initialUser(user) {
+        if (user) {
+            setCurrentUser({ ...user });
+            setUserLoggedIn(true);
+        } else {
+            setCurrentUser(null);
+            setUserLoggedIn(false);
+        }
+        setLoading(false);
+    }
+
+    const value = {
+        currentUser,
+        userLoggedIn,
+        loading
+    }   
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
+
+
+export default AuthProvider;
---
+++ src/firebase/auth.js
@@ -0,0 +1,29 @@
+import { auth } from "../firebase/firebase";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+
+export const Register = async (email, password) => {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    await signOut(auth);
+    return userCredential.user; 
+};
+
+export const  Login = async (email, password) => {
+    return await signInWithEmailAndPassword(auth, email, password);
+}
+
+
+export const signInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    return result.user;
+};
+
+export const signOut = async () => {
+    return await auth.signOut();
+};
+
+
+
+
